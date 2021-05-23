534013e2031a474eb06564f7abc49f52563c5705
Christian Segundo <christian.segundo.next@bbva.com>
2021-05-23 10:56:25 +0200

    cleanup old check

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,6 @@
 
 USER=rtorrent
 
-if [ ! -f /home/${USER}/.rtorrent.rc ]; then
-    echo "rtorrent.rc file not found! Check README"
-    exit 1
-fi
-
 chmod 777 -R /home/${USER}/rtorrent
 
 su - ${USER} -c rtorrent &
